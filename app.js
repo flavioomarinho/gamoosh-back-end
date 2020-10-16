bb1303dbba636c2131c2b704df8f76e4755538e4
flavioomarinho <flavioomarinho@hotmail.com>
2020-10-16 14:23:25 -0300

    Update app.js

--- app.js
+++ app.js
@@ -57,8 +57,8 @@ app.post('/input', (req, res) => {
 
 app.post('/recebe', (req, res) => {
 
-    paramRecebidos[i] = req.body;
-    serie = paramRecebidos[1].serie;
+    paramRecebidos[i] = req;
+    serie = paramRecebidos[1].body.serie;
     console.log(serie); 
     i++;
     /**  
