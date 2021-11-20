51c3a5cb99b65f36709d352203a243d0b8404603
flavioomarinho <flavioomarinho@hotmail.com>
2021-11-20 15:34:33 -0300

    das
    sda

--- index.js
+++ index.js
@@ -4,11 +4,13 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const config = require('./config');
 const taskRoutes = require('./routes/task-routes');
+const rewardRoutes = require('./routes/reward-routes');
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 app.use(bodyParser.json());
 app.use('/api', taskRoutes.routes);
+app.use('/api', rewardRoutes.routes);
 
 app.listen(config.port, () => console.log('App is listening on url http'+config.port));
